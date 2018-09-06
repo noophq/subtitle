83346089e331de3ce29a9a2d6c7ef18fe74199d0
pauloribeiro-hotmart <26880162+pauloribeiro-hotmart@users.noreply.github.com>
2018-09-06 14:56:16 -0300

    adding support for concurrency on chunks list (#4)

--- src/main/java/com/hotmart/subtitle/util/AudioTranscription.java
+++ src/main/java/com/hotmart/subtitle/util/AudioTranscription.java
@@ -1,17 +1,23 @@
 package com.hotmart.subtitle.util;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 import lombok.Getter;
 
 @Getter
 public class AudioTranscription {
 
-	private List<AudioTranscriptionChunk> chunks;
+	private Set<AudioTranscriptionChunk> chunks;
 	
 	public AudioTranscription() {
-		chunks = new ArrayList<>();
+		chunks = new ConcurrentSkipListSet<>(new Comparator<AudioTranscriptionChunk>() {
+			@Override
+			public int compare(AudioTranscriptionChunk o1, AudioTranscriptionChunk o2) {
+				return o1.getStartTime().compareTo(o2.getStartTime());
+			}
+		});
 	}
 
 	public void addChunk(AudioTranscriptionChunk chunk) {
